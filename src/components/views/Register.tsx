0d5d5f84496caab88fdeca52746f9f42e0ff4914
noah.isaak <noah.isaak@uzh.ch>
2024-02-27 15:02:50 +0100

    Minor Formatting Change

--- src/components/views/Register.tsx
+++ src/components/views/Register.tsx
@@ -8,106 +8,106 @@ import BaseContainer from "components/ui/BaseContainer";
 import PropTypes from "prop-types";
 
 const FormField = (props) => {
-    return (
-        <div className="register field">
-            <label className="register label">{props.label}</label>
-            <input
-                className="register input"
-                placeholder="enter here.."
-                value={props.value}
-                onChange={(e) => props.onChange(e.target.value)}
-            />
-        </div>
-    );
+  return (
+    <div className="register field">
+      <label className="register label">{props.label}</label>
+      <input
+        className="register input"
+        placeholder="enter here.."
+        value={props.value}
+        onChange={(e) => props.onChange(e.target.value)}
+      />
+    </div>
+  );
 };
 
 FormField.propTypes = {
-    label: PropTypes.string,
-    value: PropTypes.string,
-    onChange: PropTypes.func,
+  label: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
 };
 
 const Register = () => {
-    const navigate = useNavigate();
-    const [name, setName] = useState<string>(null);
-    const [username, setUsername] = useState<string>(null);
+  const navigate = useNavigate();
+  const [name, setName] = useState<string>(null);
+  const [username, setUsername] = useState<string>(null);
 
-    const doRegister = async () => {
-        try {
-            const requestBody = JSON.stringify({ username, name });
-            const response = await api.post("/users", requestBody);
+  const doRegister = async () => {
+    try {
+      const requestBody = JSON.stringify({ username, name });
+      const response = await api.post("/users", requestBody);
 
-            // Get the returned user and update a new object.
-            const user = new User(response.data);
+      // Get the returned user and update a new object.
+      const user = new User(response.data);
 
-            // Store the token into the local storage.
-            localStorage.setItem("token", user.token);
+      // Store the token into the local storage.
+      localStorage.setItem("token", user.token);
 
-            alert(
-                '${username} successfully registrated! Navigating to login page...'
-            )
+      alert(
+        `${username} successfully registrated! Navigating to login page...`
+      )
 
-            setTimeout(() => {
-                navigate("/login")
-            }, 1000);
+      setTimeout(() => {
+        navigate("/login")
+      }, 1000);
 
-            //Navigate to login page
-        } catch (error) {
-            alert(
-                `Something went wrong during the registration: \n${handleError(error)}`
-            );
-
-        }
-    };
-
-    //Create switch functionality to switch to registration form
-    const doSwitch = () => {
-        try {
-            navigate("/login");
-        }
-        catch (error) {
-            alert(
-                `Could not switch to login form: \n${handleError(error)}`
-            );
-        }
-    };
-
-    return (
-        <BaseContainer>
-          <div className="register container">
-            <div className="register form">
-              <FormField
-                label="Username"
-                value={username}
-                onChange={(un: string) => setUsername(un)}
-              />
-              <FormField
-                label="Name"
-                value={name}
-                onChange={(n) => setName(n)}
-              />
-              {/*Create button to switch to registration form*/}
-              <div className="switch login-register">
-                <Button
-                  width="100%"
-                  onClick={() => doSwitch()}
-                  >
-                  Switch
-                </Button>
-              </div>
-              <div className="register button-container">
-                <Button
-                  disabled={!username || !name}
-                  width="100%"
-                  onClick={() => doRegister()}
-                >
-                  Register
-                </Button>
-              </div>
-            </div>
-          </div>
-        </BaseContainer>
+      //Navigate to login page
+    } catch (error) {
+      alert(
+        `Something went wrong during the registration: \n${handleError(error)}`
       );
-    };
+
+    }
+  };
+
+  //Create switch functionality to switch to registration form
+  const doSwitch = () => {
+    try {
+      navigate("/login");
+    }
+    catch (error) {
+      alert(
+        `Could not switch to login form: \n${handleError(error)}`
+      );
+    }
+  };
+
+  return (
+    <BaseContainer>
+      <div className="register container">
+        <div className="register form">
+          <FormField
+            label="Username"
+            value={username}
+            onChange={(un: string) => setUsername(un)}
+          />
+          <FormField
+            label="Name"
+            value={name}
+            onChange={(n) => setName(n)}
+          />
+          {/*Create button to switch to registration form*/}
+          <div className="switch login-register">
+            <Button
+              width="100%"
+              onClick={() => doSwitch()}
+            >
+              Switch
+            </Button>
+          </div>
+          <div className="register button-container">
+            <Button
+              disabled={!username || !name}
+              width="100%"
+              onClick={() => doRegister()}
+            >
+              Register
+            </Button>
+          </div>
+        </div>
+      </div>
+    </BaseContainer>
+  );
+};
 
 export default Register;
